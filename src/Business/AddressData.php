86db8972d967718465311cc3931e94f95b7616b8
Nenad Ticaric <nticaric@gmail.com>
2014-07-12 18:33:46 +0200

    address data

---
+++ src/Business/AddressData.php
@@ -0,0 +1,31 @@
+<?php namespace Nticaric\Fiskalizacija\Business;
+
+use XMLWriter;
+
+class AddressData
+{
+
+	public $adress;
+
+	public function setAddress(Address $address)
+	{
+		$this->address = $address;
+	}
+
+
+	public function toXML()
+	{
+		$ns = 'tns';
+
+		$writer = new XMLWriter();
+    	$writer->openMemory();
+ 
+    	$writer->setIndent(true);
+    	$writer->setIndentString("    ");
+		$writer->startElementNs($ns, 'AdresniPodatak', null);
+		$writer->writeRaw($this->address->toXML());
+		$writer->endElement();
+
+		return $writer->outputMemory();
+	}
+}
