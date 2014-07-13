e681891a2c5b8f64d716b9b81ab168662bf12229
Nenad Ticaric <nticaric@gmail.com>
2014-07-13 04:04:12 +0200

    make request to the server

--- src/Fiskalizacija.php
+++ src/Fiskalizacija.php
@@ -9,12 +9,21 @@
 * @project Fiskalizacija
 */
 
+use DOMDocument, DOMElement, Exception;
+
 class Fiskalizacija {
 
 	private $uuid;
 	//privatni kljuc iz certifikata
 	private $pk;
 	public $certificate;
+
+	public function __construct($path, $pass)
+	{
+		$this->setCertificate($path, $pass);
+		$this->privateKeyResource = openssl_pkey_get_private($this->certificate['pkey'], $pass);
+		$this->publicCertificateData = openssl_x509_parse($this->certificate['cert']);
+	}
 
 	public function setCertificate($path, $pass)
 	{
@@ -65,6 +74,147 @@ class Fiskalizacija {
 		$medjurezultat .= $onu;
 		$medjurezultat .= $uir;
 		return md5($medjurezultat);
+	}
+
+	public function signXML($XMLRequest)
+	{
+		$XMLRequestDOMDoc = new DOMDocument();
+		$XMLRequestDOMDoc->loadXML($XMLRequest);
+
+		$canonical = $XMLRequestDOMDoc->C14N();
+		$DigestValue = base64_encode(hash('sha1', $canonical, true));
+
+		$rootElem = $XMLRequestDOMDoc->documentElement;
+
+		$SignatureNode = $rootElem->appendChild(new DOMElement('Signature'));
+		$SignatureNode->setAttribute('xmlns','http://www.w3.org/2000/09/xmldsig#');
+
+		$SignedInfoNode = $SignatureNode->appendChild(new DOMElement('SignedInfo'));
+		$SignedInfoNode->setAttribute('xmlns','http://www.w3.org/2000/09/xmldsig#');
+
+		$CanonicalizationMethodNode = $SignedInfoNode->appendChild(new DOMElement('CanonicalizationMethod'));
+		$CanonicalizationMethodNode->setAttribute('Algorithm','http://www.w3.org/2001/10/xml-exc-c14n#');
+
+		$SignatureMethodNode = $SignedInfoNode->appendChild(new DOMElement('SignatureMethod'));
+		$SignatureMethodNode->setAttribute('Algorithm','http://www.w3.org/2000/09/xmldsig#rsa-sha1');
+
+		$ReferenceNode = $SignedInfoNode->appendChild(new DOMElement('Reference'));
+		$ReferenceNode->setAttribute('URI', sprintf('#%s', $XMLRequestDOMDoc->documentElement->getAttribute('Id')));
+
+		$TransformsNode = $ReferenceNode->appendChild(new DOMElement('Transforms'));
+
+		$Transform1Node = $TransformsNode->appendChild(new DOMElement('Transform'));
+		$Transform1Node->setAttribute('Algorithm','http://www.w3.org/2000/09/xmldsig#enveloped-signature');
+
+		$Transform2Node = $TransformsNode->appendChild(new DOMElement('Transform'));
+		$Transform2Node->setAttribute('Algorithm', 'http://www.w3.org/2001/10/xml-exc-c14n#');
+
+		$DigestMethodNode = $ReferenceNode->appendChild(new DOMElement('DigestMethod'));
+		$DigestMethodNode->setAttribute('Algorithm','http://www.w3.org/2000/09/xmldsig#sha1');
+
+		$ReferenceNode->appendChild(new DOMElement('DigestValue', $DigestValue));
+
+		$SignedInfoNode = $XMLRequestDOMDoc->getElementsByTagName('SignedInfo')->item(0);
+
+		$X509Issuer = $this->publicCertificateData['issuer'];
+		$X509IssuerName = sprintf('OU=%s,O=%s,C=%s', $X509Issuer['OU'], $X509Issuer['O'], $X509Issuer['C']);
+		$X509IssuerSerial = $this->publicCertificateData['serialNumber'];
+
+		$publicCertificatePureString = str_replace('-----BEGIN CERTIFICATE-----', '', $this->certificate['cert']);
+		$publicCertificatePureString = str_replace('-----END CERTIFICATE-----', '', $publicCertificatePureString);
+
+		$this->signedInfoSignature = null;
+
+		if (!openssl_sign($SignedInfoNode->C14N(true), $this->signedInfoSignature, $this->privateKeyResource, OPENSSL_ALGO_SHA1)) {
+		    throw new Exception('Unable to sign the request');
+		}
+
+		$SignatureNode = $XMLRequestDOMDoc->getElementsByTagName('Signature')->item(0);
+		$SignatureValueNode = new DOMElement('SignatureValue', base64_encode($this->signedInfoSignature));
+		$SignatureNode->appendChild($SignatureValueNode);
+
+		$KeyInfoNode = $SignatureNode->appendChild(new DOMElement('KeyInfo'));
+
+		$X509DataNode = $KeyInfoNode->appendChild(new DOMElement('X509Data'));
+		$X509CertificateNode = new DOMElement('X509Certificate', $publicCertificatePureString);
+		$X509DataNode->appendChild($X509CertificateNode);
+
+		$X509IssuerSerialNode = $X509DataNode->appendChild(new DOMElement('X509IssuerSerial'));
+
+		$X509IssuerNameNode = new DOMElement('X509IssuerName',$X509IssuerName);
+		$X509IssuerSerialNode->appendChild($X509IssuerNameNode);
+
+		$X509SerialNumberNode = new DOMElement('X509SerialNumber',$X509IssuerSerial);
+		$X509IssuerSerialNode->appendChild($X509SerialNumberNode);
+
+		$envelope = new DOMDocument();
+
+		$envelope->loadXML('<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+		    <soapenv:Body></soapenv:Body>
+		</soapenv:Envelope>');
+
+		$envelope->encoding = 'UTF-8';
+		$envelope->version = '1.0';
+		$XMLRequestType = $XMLRequestDOMDoc->documentElement->localName;
+		$XMLRequestTypeNode = $XMLRequestDOMDoc->getElementsByTagName($XMLRequestType)->item(0);
+		$XMLRequestTypeNode = $envelope->importNode($XMLRequestTypeNode, true);
+
+		$envelope->getElementsByTagName('Body')->item(0)->appendChild($XMLRequestTypeNode);
+		return $envelope->saveXML();
+	}
+
+	public function sendSoap($payload)
+	{
+		$ch = curl_init();
+
+		$options = array(
+		    CURLOPT_URL => 'https://cistest.apis-it.hr:8449/FiskalizacijaServiceTest',
+		    CURLOPT_CONNECTTIMEOUT => 5,
+		    CURLOPT_TIMEOUT => 5,
+		    CURLOPT_RETURNTRANSFER => true,
+		    CURLOPT_POST => true,
+		    CURLOPT_POSTFIELDS => $payload,
+		    CURLOPT_SSL_VERIFYHOST => 2,
+		    CURLOPT_SSL_VERIFYPEER => true,
+		    CURLOPT_CAINFO => './tests/democacert.cer.pem',
+		);
+
+		curl_setopt_array($ch, $options);
+		$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		$response = curl_exec($ch);
+		curl_close($ch);
+		if($response) {
+			return $this->parseResponse($response, $code);
+		} else {
+		    throw new Exception(curl_error($ch));
+		}
+		
+	}
+
+	public function parseResponse($response, $code = 4)
+	{
+	    $DOMResponse = new DOMDocument();
+	    $DOMResponse->loadXML($response);
+
+	    if ($code === 200 || $code == 0) {
+	        /* For RacunZahtjev */
+	        $jir = $DOMResponse->getElementsByTagName('Jir')->item(0);
+	        if ($jir) {
+	            return $jir->nodeValue;
+	        }
+	        /* For RacunZahtjev && PoslovniProstorZahtjev */
+	        return $response;
+	    } else {
+	        $SifraGreske = $DOMResponse->getElementsByTagName('SifraGreske')->item(0);
+	        $PorukaGreske = $DOMResponse->getElementsByTagName('PorukaGreske')->item(0);
+
+	        if ($SifraGreske && $PorukaGreske) {
+	            throw new Exception(sprintf('%s: %s', $SifraGreske->nodeValue, $PorukaGreske->nodeValue));
+	        } else {
+	            throw new Exception(print_r($response, true), $code);
+	        }
+	    }
+
 	}
 
 }
--- tests/FiskalizacijaTest.php
+++ tests/FiskalizacijaTest.php
@@ -1,19 +1,24 @@
 <?php
 
 use Nticaric\Fiskalizacija\Fiskalizacija;
+use Nticaric\Fiskalizacija\Business\Address;
+use Nticaric\Fiskalizacija\Business\AddressData;
+use Nticaric\Fiskalizacija\Business\BusinessArea;
+use Nticaric\Fiskalizacija\Business\BusinessAreaRequest;
+use Carbon\Carbon;
 
 class FiskalizacijaTest extends \PHPUnit_Framework_TestCase
 {
     public function testGenerateUUID()
     {
-    	$fis = new Fiskalizacija;
+    	$fis = new Fiskalizacija("./tests/demo.pfx", "TNTStudi0");
     	$res = $fis->generateUUID();
     	$this->assertRegExp("/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/", $res, 'Invalid UUID');
     }
 
     public function testReadCertificateFromDisk()
     {
-    	$fis = new Fiskalizacija;
+    	$fis = new Fiskalizacija("./tests/demo.pfx", "TNTStudi0");
     	$pathToDemoCert = "./tests/demo.pfx";
     	$res = $fis->readCertificateFromDisk($pathToDemoCert);
     	$this->assertTrue($res != false);
@@ -21,7 +26,7 @@ class FiskalizacijaTest extends \PHPUnit_Framework_TestCase
 
     public function testSetCertificate()
     {
-    	$fis = new Fiskalizacija;
+    	$fis = new Fiskalizacija("./tests/demo.pfx", "TNTStudi0");
     	$pathToDemoCert = "./tests/demo.pfx";
     	$fis->setCertificate($pathToDemoCert, "TNTStudi0");
     	$this->assertNotNull($fis->certificate, 'Certificate must not be null');
@@ -29,9 +34,58 @@ class FiskalizacijaTest extends \PHPUnit_Framework_TestCase
 
     public function testSetCertificateWithWrongPassword()
     {
-    	$fis = new Fiskalizacija;
-    	$pathToDemoCert = "./tests/demo.pfx";
-    	$fis->setCertificate($pathToDemoCert, "wrong_password");
+    	$fis = new Fiskalizacija("./tests/demo.pfx", "wrong_password");
     	$this->assertNull($fis->certificate, 'Certificate must not be null');
+    }
+
+    public function testSignXML()
+    {
+        $businessAreaRequest = $this->setBusinessAreaRequest();
+
+        $fis = new Fiskalizacija("./tests/demo.pfx", "TNTStudi0");
+        $soapMessage = $fis->signXML($businessAreaRequest->toXML());
+
+        $this->assertNotNull($soapMessage);
+
+    }
+
+    public function testSendSoap()
+    {
+        $businessAreaRequest = $this->setBusinessAreaRequest();
+
+        $fis = new Fiskalizacija("./tests/demo.pfx", "TNTStudi0");
+        $soapMessage = $fis->signXML($businessAreaRequest->toXML());
+
+        $res = $fis->sendSoap($soapMessage);
+        $this->assertContains('PoslovniProstorOdgovor', $res);
+    }
+
+    public function setBusinessAreaRequest()
+    {
+        $address = new Address;
+        $address->street = "Sv. Mateja";
+        $address->houseNumber = "19";
+        $address->zipCode = "10000";
+        $address->settlement = "Zagreb";
+        $address->city = "Zagreb";
+
+        $addressData = new AddressData;
+        $addressData->setAddress($address);
+
+        $businessArea = new BusinessArea;
+        $businessArea->setAddressData($addressData);
+
+        $date = Carbon::now()->format("d.m.Y");
+        $businessArea->setDateOfusage($date);
+
+        $businessArea->setNoteOfBusinessArea("ODV1");
+        //$businessArea->setNoteOfClosing("Z");
+        $businessArea->setOib("32314900695");
+        $businessArea->setSpecificPurpose("spec namjena");
+        
+        $businessArea->setWorkingTime("Pon:08-11h Uto:15-17");
+        $businessAreaRequest = new BusinessAreaRequest($businessArea);
+
+        return $businessAreaRequest;
     }
 }
