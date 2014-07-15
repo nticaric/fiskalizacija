05ca67f9f6a56e1f09ca6286cb3137303879dbf1
Nenad Ticaric <nticaric@gmail.com>
2014-07-15 17:00:25 +0200

    Refund Class and test

--- src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-	'oib'             => '32314900695',
-    'passphrase'      => 'TNTStudi0',
-    'certificatePath' => 'demo.pfx'
-);
-
-?>
---
+++ tests/Bill/RefundTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Nticaric\Fiskalizacija\Bill\Refund;
+
+class RefundTest extends \PHPUnit_Framework_TestCase
+{
+	public function testRefundClass()
+	{
+		$refund = new Refund("Povratna naknada", 1.23);
+		$res = $refund->toXML();
+		$this->assertStringEqualsFile('./tests/xml/business_refund.xml', $res);
+	}
+}
