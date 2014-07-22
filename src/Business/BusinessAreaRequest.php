8ed4d12f0a3ee7aa9a95c1203b92575b66d3d663
Nenad Ticaric <nticaric@gmail.com>
2014-07-22 14:28:02 +0200

    refactoring

--- src/Business/BusinessAreaRequest.php
+++ src/Business/BusinessAreaRequest.php
@@ -5,34 +5,9 @@ use XMLWriter;
 
 class BusinessAreaRequest extends Request
 {
-
-    public $businessArea;
-    
     public function __construct(BusinessArea $businessArea)
     {
-        $this->businessArea = $businessArea;
-    }
-
-    public function toXML()
-    {
-        $ns = 'tns';
-
-        $writer = new XMLWriter();
-        $writer->openMemory();
- 
-        $writer->setIndent(true);
-        $writer->setIndentString("    ");
-        $writer->startElementNs($ns, 'PoslovniProstorZahtjev', 'http://www.apis-it.hr/fin/2012/types/f73');
-        $writer->writeAttribute('Id', uniqid());
-        $writer->startElementNs($ns, 'Zaglavlje', null);
-        $writer->writeElementNs($ns, 'IdPoruke', null, $this->generateUUID());
-        $writer->writeElementNs($ns, 'DatumVrijeme', null, \Carbon\Carbon::now()->format('d.m.Y\Th:i:s'));
-        $writer->endElement();
-
-        //PoslovniProstor
-        $writer->writeRaw($this->businessArea->toXML());
-        $writer->endElement();
-        
-        return $writer->outputMemory();
+        $this->request     = $businessArea;
+        $this->requestName = 'PoslovniProstorZahtjev';
     }
 }
--- src/Request.php
+++ src/Request.php
@@ -1,6 +1,11 @@
 <?php namespace Nticaric\Fiskalizacija;
 
+use XMLWriter;
+
 class Request {
+
+    protected $requestName;
+    protected $request;
 
     public function generateUUID() 
     {
@@ -11,5 +16,27 @@ class Request {
             mt_rand(0, 0x3fff) | 0x8000,
             mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
         );
+    }
+
+    public function toXML()
+    {
+        $ns = 'tns';
+
+        $writer = new XMLWriter();
+        $writer->openMemory();
+ 
+        $writer->setIndent(true);
+        $writer->setIndentString("    ");
+        $writer->startElementNs($ns, $this->requestName, 'http://www.apis-it.hr/fin/2012/types/f73');
+        $writer->writeAttribute('Id', uniqid());
+        $writer->startElementNs($ns, 'Zaglavlje', null);
+        $writer->writeElementNs($ns, 'IdPoruke', null, $this->generateUUID());
+        $writer->writeElementNs($ns, 'DatumVrijeme', null, \Carbon\Carbon::now()->format('d.m.Y\Th:i:s'));
+        $writer->endElement();
+
+        $writer->writeRaw($this->request->toXML());
+        $writer->endElement();
+        
+        return $writer->outputMemory();
     }
 }
